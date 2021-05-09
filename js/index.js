a4f9d92b43fb210c0de0821d754ab15eb3a5c792
LeoPavani <leonardopavani.lp@hotmail.com>
2021-05-09 18:41:29 -0300

    Arrumando o problema de Scroll lateral.

--- js/index.js
+++ js/index.js
@@ -2,9 +2,15 @@ const botoes = document.getElementsByClassName("leia-mais-botao");
 for (const botao of botoes) {
     botao.addEventListener("click", function( event ) {
         const title = botao.getAttribute("data-title");
-        const content = botao.getAttribute("data-content");
-
         $("#titleModal").text(title);
+
+        const content = botao.getAttribute("data-content");
         $("#contentModal").html(content);
+
+        const src = botao.getAttribute("data-src");
+        if(!!$("#imageModal")){
+            $("#imageModal").attr("src", src);
+        }
+        
     });   
 }
